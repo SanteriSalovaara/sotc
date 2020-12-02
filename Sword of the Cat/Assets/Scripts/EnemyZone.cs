ad98c51e87f3526f90d035454062a3bbf6a195af
Eemeli Saarinen <saarineneemelia@gmail.com>
2020-12-02 13:58:32 +0200

    Enemy aggro zone

---
+++ Sword of the Cat/Assets/Scripts/EnemyZone.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyZone : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision) {
+        GameObject go = collision.gameObject;
+        // Check if the entity entering the zone is a player
+        if (go.tag == "Player") {
+            // Loop all child-gameobjects and check if they are enemies
+            foreach(Transform childTransform in this.transform) {
+                GameObject child = childTransform.gameObject;
+                if (child.tag == "Enemy") {
+                    // Make all enemies in the zone start attacking
+                    child.GetComponent<EnemyController>().hasAggro = true;
+                }
+            }
+        }
+    }
+
+    void OnCollisionExit(Collision collision) {
+        GameObject go = collision.gameObject;
+        // Check if the entity entering the zone is a player
+        if (go.tag == "Player") {
+            // Loop all child-gameobjects and check if they are enemies
+            foreach(Transform childTransform in this.transform) {
+                GameObject child = childTransform.gameObject;
+                if (child.tag == "Enemy") {
+                    // Make all enemies in the zone stop attacking
+                    child.GetComponent<EnemyController>().hasAggro = false;
+                }
+            }
+        }
+    }
+}
